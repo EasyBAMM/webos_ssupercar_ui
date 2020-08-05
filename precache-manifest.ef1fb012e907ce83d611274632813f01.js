7ca996c0e6e8ccfdead733f99ea6c14f0571e884
EasyBAMM <huhjb1020@naver.com>
2020-08-06 02:37:28 +0900

    Updates

---
+++ precache-manifest.ef1fb012e907ce83d611274632813f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e8c3a08af2d03aec2147949e84d23d",
+    "url": "/webos_ssupercar_ui/index.html"
+  },
+  {
+    "revision": "0e7b03ca8babab489d5c",
+    "url": "/webos_ssupercar_ui/static/css/main.b1d125d1.chunk.css"
+  },
+  {
+    "revision": "d3e2f078868fbf669cf8",
+    "url": "/webos_ssupercar_ui/static/js/2.70134ac8.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/webos_ssupercar_ui/static/js/2.70134ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7b03ca8babab489d5c",
+    "url": "/webos_ssupercar_ui/static/js/main.7d4290d6.chunk.js"
+  },
+  {
+    "revision": "e105b5c2066bf93c2c79",
+    "url": "/webos_ssupercar_ui/static/js/runtime-main.53fa7036.js"
+  },
+  {
+    "revision": "10ce37aad862e69e48c2ca481cee7ed9",
+    "url": "/webos_ssupercar_ui/static/media/sign1.10ce37aa.png"
+  }
+]);
